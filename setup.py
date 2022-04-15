65cea0581fda7793c7dd3539039b6593ae52c798
Sai Sharath Kumar Sabbarapu <98193657+SSharath-Kumar@users.noreply.github.com>
2022-04-15 03:00:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 	name='project2',
 	version='1.0',
 	author='Sai Sharath Kumar, Sabbarapu',
-	authour_email='s.sharat.kumar@ou.edu',
+	authour_email='s.sharath.kumar@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']
